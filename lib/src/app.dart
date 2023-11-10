9f95e498c8a38c64249af1913ced44c1bf31c8e3
Hadi <hadim69@gmail.com>
2023-11-10 10:25:46 +0330

    Add app.dart file.

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Crypto Wallet',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text("Text Data"),
+        ),
+      ),
+    );
+  }
+}
