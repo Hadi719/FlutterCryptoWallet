32b2a1fb683935baba3b71eed5774dad7f9e4237
Hadi <hadim69@gmail.com>
2023-11-10 14:38:46 +0330

    Add string constants file.

---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,9 @@
+// App
+const String kAppTitle = 'Flutter Crypto Wallet';
+
+// Networking & APIs
+const String kCoinExScheme = 'https';
+const String kCoinExHost = 'api.coinex.com';
+const String kCoinExInitialPath = '/v1';
+
+// Storage and Databases
