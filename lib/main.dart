a5f1c0cf86b1afe2fefe498c10f2f30d47436cc4
Hadi <hadim69@gmail.com>
2023-11-10 10:27:53 +0330

    Initialize Firebase and Auth.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,40 @@
-void main() {
-  // runApp(const MyApp());
+// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'src/app.dart';
+import 'src/config/firebase/firebase_options.dart';
+
+/// Requires that a Firebase local emulator is running locally.
+/// See https://firebase.flutter.dev/docs/auth/start/#optional-prototype-and-test-with-firebase-local-emulator-suite
+// bool shouldUseFirebaseEmulator = true;
+
+late final FirebaseApp app;
+late final FirebaseAuth auth;
+
+// Requires that the Firebase Auth emulator is running locally
+// e.g via `melos run firebase:emulator`.
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // We're using the manual installation on non-web platforms since Google sign in plugin doesn't yet support Dart initialization.
+  // See related issue: https://github.com/flutter/flutter/issues/96391
+
+  // We store the app and auth to make testing with a named instance easier.
+  app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  auth = FirebaseAuth.instanceFor(app: app);
+
+  // Active Firebase Auth Emulator
+  /*
+  if (shouldUseFirebaseEmulator) {
+    await auth.useAuthEmulator('localhost', 9099);
+  }
+  */
+
+  runApp(const MyApp());
 }
