c47058d53687847bdbbd873a3e41de4996be5c10
Hadi <hadim69@gmail.com>
2023-11-11 21:21:09 +0330

    remove scheme.

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -2,7 +2,6 @@
 const String kAppTitle = 'Flutter Crypto Wallet';
 
 // Networking & APIs
-const String kCoinExScheme = 'https';
 const String kCoinExHost = 'api.coinex.com';
 const String kCoinExInitialPath = '/v1';
 
