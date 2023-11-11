27707f8f7d349f96837706faef946fd08f6d12ae
Hadi <hadim69@gmail.com>
2023-11-11 21:24:30 +0330

    update

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -3,6 +3,6 @@ const String kAppTitle = 'Flutter Crypto Wallet';
 
 // Networking & APIs
 const String kCoinExHost = 'api.coinex.com';
-const String kCoinExInitialPath = '/v1';
+const String kCoinExBaseUrl = '/v1';
 
 // Storage and Databases
