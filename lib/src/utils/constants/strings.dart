7ea5f74146945984768d009be9d20316c0270598
Hadi <hadim69@gmail.com>
2023-11-12 14:58:13 +0330

    add All & Single market info url path.

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -4,5 +4,7 @@ const String kAppTitle = 'Flutter Crypto Wallet';
 // Networking & APIs
 const String kCoinExBaseUrl = 'https://api.coinex.com/v1';
 const String kCoinExUrlPathAllMarketList = '/market/list';
+const String kCoinExUrlPathAllMarketInfo = '/market/info';
+const String kCoinExUrlPathSingleMarketInfo = '/market/detail';
 
 // Storage and Databases
