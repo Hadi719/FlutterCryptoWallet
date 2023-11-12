4ffcc3d5076cce28812c35b05e8e4448ca5270ee
Hadi <hadim69@gmail.com>
2023-11-12 13:45:59 +0330

    Add Dio and CoinEx.

--- lib/src/service_locator.dart
+++ lib/src/service_locator.dart
@@ -1,5 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
+import 'data/datasource/remote/coin_ex_api_service.dart';
+import 'data/repositories/api_repository_impl.dart';
+import 'domain/repositories/api_repository.dart';
 
 final GetIt serviceLocator = GetIt.instance;
 
-Future<void> initializeDependencies() async {}
+Future<void> initializeDependencies() async {
+  final dio = Dio();
+
+  serviceLocator.registerSingleton<Dio>(dio);
+
+  serviceLocator.registerSingleton<CoinExApiService>(
+    CoinExApiService(serviceLocator<Dio>()),
+  );
+
+  serviceLocator.registerSingleton<ApiRepository>(
+    ApiRepositoryImpl(serviceLocator<CoinExApiService>()),
+  );
+}
