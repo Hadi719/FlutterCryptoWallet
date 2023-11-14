7040ad4ac99c3a0730fcebba02fc3a49bab73879
Hadi <hadim69@gmail.com>
2023-11-14 20:00:01 +0330

    Add market depth url path.

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -6,5 +6,6 @@ const String kCoinExBaseUrl = 'https://api.coinex.com/v1';
 const String kCoinExUrlPathAllMarketList = '/market/list';
 const String kCoinExUrlPathAllMarketInfo = '/market/info';
 const String kCoinExUrlPathSingleMarketInfo = '/market/detail';
+const String kCoinExUrlPathMarketDepth = '/market/depth';
 
 // Storage and Databases
