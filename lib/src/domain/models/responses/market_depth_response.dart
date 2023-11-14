f4e970ba90329116820962f1850aef79221569a3
Hadi <hadim69@gmail.com>
2023-11-14 21:20:09 +0330

    Add market depth response.

---
+++ lib/src/domain/models/responses/market_depth_response.dart
@@ -0,0 +1,74 @@
+import 'package:equatable/equatable.dart';
+
+/// {@template MarketDepthResponse}
+///
+/// Gets market depth in a single market.
+///
+/// * Signature required: No
+///
+/// {@endtemplate}
+class MarketDepthResponse extends Equatable {
+  final DateTime time;
+  final double latestTransactionPrice;
+  final List<MarketDepthOrder> askOrders;
+  final List<MarketDepthOrder> bidOrders;
+
+  /// {@macro MarketDepthResponse}
+  const MarketDepthResponse({
+    required this.time,
+    required this.latestTransactionPrice,
+    this.askOrders = const <MarketDepthOrder>[],
+    this.bidOrders = const <MarketDepthOrder>[],
+  });
+
+  /// {@macro MarketDepthResponse}
+  factory MarketDepthResponse.fromMap(Map<String, dynamic> map) {
+    Map<String, dynamic> data = map['data'];
+    return MarketDepthResponse(
+      time: DateTime.fromMillisecondsSinceEpoch(data['time']),
+      latestTransactionPrice: double.tryParse(data['last']) ?? 0.0,
+      askOrders: (data['asks'] as List)
+          .map((e) => MarketDepthOrder.fromJson(e))
+          .toList(),
+      bidOrders: (data['bids'] as List)
+          .map((e) => MarketDepthOrder.fromJson(e))
+          .toList(),
+    );
+  }
+
+  @override
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [];
+}
+
+class MarketDepthOrder extends Equatable {
+  final double price;
+  final double amount;
+
+  const MarketDepthOrder({
+    this.price = 0.0,
+    this.amount = 0.0,
+  });
+
+  factory MarketDepthOrder.fromJson(List<String> jsonData) {
+    return MarketDepthOrder(
+      price: double.tryParse(jsonData[0][0]) ?? 0.0,
+      amount: double.tryParse(jsonData[0][1]) ?? 0.0,
+    );
+  }
+
+  @override
+  String toString() {
+    return '\n\n=======   $this   =======\n'
+        'Order [price:$price, amount:$amount]\n'
+        '=========================================';
+  }
+
+  @override
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [price, amount];
+}
