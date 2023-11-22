c8cfb1934ba9c55b8852735854871a9ac20653a6
Hadi <hadim69@gmail.com>
2023-11-22 20:54:16 +0330

    update & refactor models.

--- lib/src/domain/models/requests/single_market_info_request.dart
+++ lib/src/domain/models/requests/single_market_info_request.dart
@@ -1,8 +1,8 @@
 /// Gets detailed information on a single market.
 class SingleMarketInfoRequest {
-  /// Gets detailed information on a single market.
-  const SingleMarketInfoRequest({required this.marketName});
-
   /// It refers to [CryptoDetail.values.marketName]
   final String marketName;
+
+  /// Gets detailed information on a single market.
+  const SingleMarketInfoRequest({required this.marketName});
 }
