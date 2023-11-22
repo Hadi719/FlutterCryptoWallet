1e2d3b2d9db330841e9754f8124c2a7fe548d517
Hadi <hadim69@gmail.com>
2023-11-22 13:02:27 +0330

    Build K-Line Data retrofit build generator.

--- lib/src/data/datasource/remote/coin_ex_api_service.g.dart
+++ lib/src/data/datasource/remote/coin_ex_api_service.g.dart
@@ -179,6 +179,43 @@ class _CoinExApiService implements CoinExApiService {
     return httpResponse;
   }
 
+  @override
+  Future<HttpResponse<KLineDataResponse>> getKLineData({
+    required String marketName,
+    int? limit,
+    required String type,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'market': marketName,
+      r'limit': limit,
+      r'type': type,
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HttpResponse<KLineDataResponse>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/market/kline',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = KLineDataResponse.fromMap(_result.data!);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
