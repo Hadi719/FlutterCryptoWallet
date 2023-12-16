4f7116b8db511e9a7c9017900206d9997c17b792
Hadi <hadim69@gmail.com>
2023-12-16 14:20:29 +0330

    use string constants.

--- lib/src/coinex_app.dart
+++ lib/src/coinex_app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../development/test_coin_ex_api.dart';
+import 'utils/constants/strings.dart';
 
 class CoinExApp extends StatelessWidget {
   const CoinExApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Crypto Wallet',
+      title: kAppTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
