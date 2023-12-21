3ae37a845eac1fad4111df2de6f9c5c35d047e09
Hadi <hadim69@gmail.com>
2023-12-21 12:10:02 +0330

    Add test coinex bloc

---
+++ lib/development/test_ce_bloc.dart
@@ -0,0 +1,340 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+import '../src/domain/models/coinex/crypto.dart';
+import '../src/domain/models/requests/requests.dart';
+import '../src/domain/models/responses/responses.dart';
+import '../src/domain/repositories/api_repository.dart';
+import '../src/service_locator.dart';
+import '../src/utils/resources/data_state.dart';
+
+part 'test_ce_event.dart';
+part 'test_ce_state.dart';
+
+class TestCeBloc extends Bloc<TestCeEvent, TestCeState> {
+  TestCeBloc() : super(const TestCeState()) {
+    on<TestCeAllMarketList>(_onTestCeAllMarketList);
+    on<TestCeAllMarketInfo>(_onTestCeAllMarketInfo);
+    on<TestCeSingleMarketInfo>(_onTestCeSingleMarketInfo);
+    on<TestCeMarketDepth>(_onTestCeMarketDepth);
+    on<TestCeLatestTransactionData>(_onTestCeLatestTransactionData);
+    on<TestCeKLineData>(_onTestCeKLineData);
+    on<TestCeSingleMarketStatistics>(_onTestCeSingleMarketStatistics);
+    on<TestCeAllMarketStatistics>(_onTestCeAllMarketStatistics);
+    on<TestCeCurrencyRate>(_onTestCeCurrencyRate);
+  }
+
+  Future<void> _onTestCeAllMarketList(
+    TestCeAllMarketList event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeAllMarketList(),
+      ));
+      final data = await RemoteCoinEx.getAllMarketList();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeAllMarketInfo(
+    TestCeAllMarketInfo event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeAllMarketInfo(),
+      ));
+      final data = await RemoteCoinEx.getAllMarketInfo();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeSingleMarketInfo(
+    TestCeSingleMarketInfo event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeSingleMarketInfo(),
+      ));
+      final data = await RemoteCoinEx.getSingleMarketInfo();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeMarketDepth(
+    TestCeMarketDepth event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeMarketDepth(),
+      ));
+      final data = await RemoteCoinEx.getMarketDepth();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeLatestTransactionData(
+    TestCeLatestTransactionData event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeLatestTransactionData(),
+      ));
+      final data = await RemoteCoinEx.getLatestTransactionData();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeKLineData(
+    TestCeKLineData event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeKLineData(),
+      ));
+      final data = await RemoteCoinEx.getKLineData();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeSingleMarketStatistics(
+    TestCeSingleMarketStatistics event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeSingleMarketStatistics(),
+      ));
+      final data = await RemoteCoinEx.getSingleMarketStatistics();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeAllMarketStatistics(
+    TestCeAllMarketStatistics event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeAllMarketStatistics(),
+      ));
+      final data = await RemoteCoinEx.getAllMarketStatistics();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+
+  Future<void> _onTestCeCurrencyRate(
+    TestCeCurrencyRate event,
+    Emitter<TestCeState> emit,
+  ) async {
+    try {
+      emit(state.copyWith(
+        status: TestCeStatus.loading,
+        lastEvent: TestCeCurrencyRate(),
+      ));
+      final data = await RemoteCoinEx.getCurrencyRate();
+      emit(state.copyWith(
+        status: TestCeStatus.success,
+        data: data,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: TestCeStatus.failure));
+    }
+  }
+}
+
+class RemoteCoinEx {
+  const RemoteCoinEx();
+
+  static Future<List<String>> getAllMarketList() async {
+    final DataState<AllMarketListResponse> response =
+        await serviceLocator<ApiRepository>().getAllMarketList(
+      request: const AllMarketListRequest(),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getAllMarketList');
+      return response.data!.data;
+    } else {
+      debugPrint('FAILED: $getAllMarketList');
+      throw response.error!;
+    }
+  }
+
+  static Future<List<SingleMarketInfoResponse>> getAllMarketInfo() async {
+    final DataState<AllMarketInfoResponse> response =
+        await serviceLocator<ApiRepository>().getAllMarketInfo(
+      request: const AllMarketInfoRequest(),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getAllMarketInfo');
+      return response.data!.data;
+    } else {
+      debugPrint('FAILED: $getAllMarketInfo');
+      throw response.error!;
+    }
+  }
+
+  static Future<SingleMarketInfoResponse> getSingleMarketInfo() async {
+    final DataState<SingleMarketInfoResponse> response =
+        await serviceLocator<ApiRepository>().getSingleMarketInfo(
+      request: SingleMarketInfoRequest(marketName: CryptoDetail.btc.marketName),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getSingleMarketInfo');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getSingleMarketInfo');
+      throw response.error!;
+    }
+  }
+
+  static Future<MarketDepthResponse> getMarketDepth() async {
+    final DataState<MarketDepthResponse> response =
+        await serviceLocator<ApiRepository>().getMarketDepth(
+      request: MarketDepthRequest(
+        marketName: CryptoDetail.btc.marketName,
+      ),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getMarketDepth');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getMarketDepth');
+      throw response.error!;
+    }
+  }
+
+  static Future<LatestTransactionDataResponse>
+      getLatestTransactionData() async {
+    final DataState<LatestTransactionDataResponse> response =
+        await serviceLocator<ApiRepository>().getLatestTransactionData(
+      request: LatestTransactionDataRequest(
+        marketName: CryptoDetail.btc.marketName,
+      ),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getLatestTransactionData');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getLatestTransactionData');
+      throw response.error!;
+    }
+  }
+
+  static Future<KLineDataResponse> getKLineData() async {
+    final DataState<KLineDataResponse> response =
+        await serviceLocator<ApiRepository>().getKLineData(
+      request: KLineDataRequest(
+        marketName: CryptoDetail.btc.marketName,
+      ),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getKLineData');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getKLineData');
+      throw response.error!;
+    }
+  }
+
+  static Future<SingleMarketStatisticsResponse>
+      getSingleMarketStatistics() async {
+    final DataState<SingleMarketStatisticsResponse> response =
+        await serviceLocator<ApiRepository>().getSingleMarketStatistics(
+      request: SingleMarketStatisticsRequest(
+        marketName: CryptoDetail.btc.marketName,
+      ),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getSingleMarketStatistics');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getSingleMarketStatistics');
+      throw response.error!;
+    }
+  }
+
+  static Future<AllMarketStatisticsResponse> getAllMarketStatistics() async {
+    final DataState<AllMarketStatisticsResponse> response =
+        await serviceLocator<ApiRepository>().getAllMarketStatistics(
+      request: const AllMarketStatisticsRequest(),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getAllMarketStatistics');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getAllMarketStatistics');
+      throw response.error!;
+    }
+  }
+
+  static Future<CurrencyRateResponse> getCurrencyRate() async {
+    final DataState<CurrencyRateResponse> response =
+        await serviceLocator<ApiRepository>().getCurrencyRate(
+      request: const CurrencyRateRequest(),
+    );
+    if (response is DataSuccess) {
+      debugPrint('SUCCESS: $getCurrencyRate');
+      return response.data!;
+    } else {
+      debugPrint('FAILED: $getCurrencyRate');
+      throw response.error!;
+    }
+  }
+}
--- lib/development/test_coin_ex_api.dart
+++ lib/development/test_coin_ex_api.dart
@@ -1,174 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../src/domain/models/coinex/crypto.dart';
-import '../src/domain/models/requests/requests.dart';
-import '../src/domain/models/responses/responses.dart';
-import '../src/domain/repositories/api_repository.dart';
-import '../src/service_locator.dart';
-import '../src/utils/resources/data_state.dart';
+import 'test_ce_bloc.dart';
 
-class TestCoinExApi extends StatefulWidget {
+class TestCoinExApi extends StatelessWidget {
   const TestCoinExApi({super.key});
 
   @override
-  State<TestCoinExApi> createState() => _TestCoinExApiState();
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: BlocProvider<TestCeBloc>(
+        create: (_) => TestCeBloc(),
+        child: const _LayoutBuilder(),
+      ),
+    );
+  }
 }
 
-class _TestCoinExApiState extends State<TestCoinExApi> {
+class _LayoutBuilder extends StatelessWidget {
+  const _LayoutBuilder();
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: const RemoteCoinEx().getCurrencyRate(),
-      builder: (BuildContext context, snapshot) {
-        return SafeArea(
-          child: SingleChildScrollView(
-            child: Text(
-              snapshot.hasData
-                  ? snapshot.data.toString()
-                  : snapshot.error.toString(),
-              textScaler: const TextScaler.linear(1.5),
+    return Column(
+      children: [
+        BlocSelector<TestCeBloc, TestCeState, TestCeEvent?>(
+          selector: (state) {
+            return state.lastEvent;
+          },
+          builder: (context, state) {
+            return Wrap(
+              children: [
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeAllMarketList
+                      ? null
+                      : () {
+                          context.read<TestCeBloc>().add(TestCeAllMarketList());
+                        },
+                  child: const Text('AllMarketList'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeAllMarketInfo
+                      ? null
+                      : () {
+                          context.read<TestCeBloc>().add(TestCeAllMarketInfo());
+                        },
+                  child: const Text('AllMarketInfo'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeSingleMarketInfo
+                      ? null
+                      : () {
+                          context
+                              .read<TestCeBloc>()
+                              .add(TestCeSingleMarketInfo());
+                        },
+                  child: const Text('SingleMarketInfo'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeMarketDepth
+                      ? null
+                      : () {
+                          context.read<TestCeBloc>().add(TestCeMarketDepth());
+                        },
+                  child: const Text('MarketDepth'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeLatestTransactionData
+                      ? null
+                      : () {
+                          context
+                              .read<TestCeBloc>()
+                              .add(TestCeLatestTransactionData());
+                        },
+                  child: const Text('LatestTransactionData'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeKLineData
+                      ? null
+                      : () {
+                          context.read<TestCeBloc>().add(TestCeKLineData());
+                        },
+                  child: const Text('KLineData'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeSingleMarketStatistics
+                      ? null
+                      : () {
+                          context
+                              .read<TestCeBloc>()
+                              .add(TestCeSingleMarketStatistics());
+                        },
+                  child: const Text('SingleMarketStatistics'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeAllMarketStatistics
+                      ? null
+                      : () {
+                          context
+                              .read<TestCeBloc>()
+                              .add(TestCeAllMarketStatistics());
+                        },
+                  child: const Text('AllMarketStatistics'),
+                ),
+                ElevatedButton(
+                  onPressed: state.runtimeType == TestCeCurrencyRate
+                      ? null
+                      : () {
+                          context.read<TestCeBloc>().add(TestCeCurrencyRate());
+                        },
+                  child: const Text('CurrencyRate'),
+                ),
+              ],
+            );
+          },
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BlocBuilder<TestCeBloc, TestCeState>(
+              builder: (BuildContext context, TestCeState state) {
+                switch (state.status) {
+                  case TestCeStatus.initial:
+                    return const Center(
+                      child: Text(
+                        'Click a button',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    );
+                  case TestCeStatus.loading:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  case TestCeStatus.failure:
+                    return const Center(
+                      child: Text(
+                        'Failed to get data',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    );
+                  case TestCeStatus.success:
+                    return SingleChildScrollView(
+                      child: Text(state.data.toString()),
+                    );
+                }
+              },
             ),
           ),
-        );
-      },
+        ),
+      ],
     );
-  }
-}
-
-class RemoteCoinEx {
-  const RemoteCoinEx();
-
-  Future<List<String>> getAllMarketList() async {
-    final DataState<AllMarketListResponse> response =
-        await serviceLocator<ApiRepository>().getAllMarketList(
-      request: const AllMarketListRequest(),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getAllMarketList');
-      return response.data!.data;
-    } else {
-      debugPrint('FAILED: $getAllMarketList');
-      throw response.error!;
-    }
-  }
-
-  Future<List<SingleMarketInfoResponse>> getAllMarketInfo() async {
-    final DataState<AllMarketInfoResponse> response =
-        await serviceLocator<ApiRepository>().getAllMarketInfo(
-      request: const AllMarketInfoRequest(),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getAllMarketInfo');
-      return response.data!.data;
-    } else {
-      debugPrint('FAILED: $getAllMarketInfo');
-      throw response.error!;
-    }
-  }
-
-  Future<SingleMarketInfoResponse> getSingleMarketInfo() async {
-    final DataState<SingleMarketInfoResponse> response =
-        await serviceLocator<ApiRepository>().getSingleMarketInfo(
-      request: SingleMarketInfoRequest(marketName: CryptoDetail.btc.marketName),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getSingleMarketInfo');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getSingleMarketInfo');
-      throw response.error!;
-    }
-  }
-
-  Future<MarketDepthResponse> getMarketDepth() async {
-    final DataState<MarketDepthResponse> response =
-        await serviceLocator<ApiRepository>().getMarketDepth(
-      request: MarketDepthRequest(
-        marketName: CryptoDetail.btc.marketName,
-      ),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getMarketDepth');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getMarketDepth');
-      throw response.error!;
-    }
-  }
-
-  Future<LatestTransactionDataResponse> getLatestTransactionData() async {
-    final DataState<LatestTransactionDataResponse> response =
-        await serviceLocator<ApiRepository>().getLatestTransactionData(
-      request: LatestTransactionDataRequest(
-        marketName: CryptoDetail.btc.marketName,
-      ),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getLatestTransactionData');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getLatestTransactionData');
-      throw response.error!;
-    }
-  }
-
-  Future<KLineDataResponse> getKLineData() async {
-    final DataState<KLineDataResponse> response =
-        await serviceLocator<ApiRepository>().getKLineData(
-      request: KLineDataRequest(
-        marketName: CryptoDetail.btc.marketName,
-      ),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getKLineData');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getKLineData');
-      throw response.error!;
-    }
-  }
-
-  Future<SingleMarketStatisticsResponse> getSingleMarketStatistics() async {
-    final DataState<SingleMarketStatisticsResponse> response =
-        await serviceLocator<ApiRepository>().getSingleMarketStatistics(
-      request: SingleMarketStatisticsRequest(
-        marketName: CryptoDetail.btc.marketName,
-      ),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getSingleMarketStatistics');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getSingleMarketStatistics');
-      throw response.error!;
-    }
-  }
-
-  Future<AllMarketStatisticsResponse> getAllMarketStatistics() async {
-    final DataState<AllMarketStatisticsResponse> response =
-        await serviceLocator<ApiRepository>().getAllMarketStatistics(
-      request: const AllMarketStatisticsRequest(),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getAllMarketStatistics');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getAllMarketStatistics');
-      throw response.error!;
-    }
-  }
-
-  Future<CurrencyRateResponse> getCurrencyRate() async {
-    final DataState<CurrencyRateResponse> response =
-        await serviceLocator<ApiRepository>().getCurrencyRate(
-      request: const CurrencyRateRequest(),
-    );
-    if (response is DataSuccess) {
-      debugPrint('SUCCESS: $getCurrencyRate');
-      return response.data!;
-    } else {
-      debugPrint('FAILED: $getCurrencyRate');
-      throw response.error!;
-    }
   }
 }
