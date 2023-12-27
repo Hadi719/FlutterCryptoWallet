d063dd14c2bc67bfd6dad45b9675747cac08477a
Hadi <hadim69@gmail.com>
2023-12-27 17:09:42 +0330

    Add AuthRep from GetIt service locator.

--- lib/src/presentation/app/bloc/app_bloc.dart
+++ lib/src/presentation/app/bloc/app_bloc.dart
@@ -4,25 +4,26 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../service_locator.dart';
+
 part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc({required AuthenticationRepository authenticationRepository})
-      : _authenticationRepository = authenticationRepository,
-        super(
-          authenticationRepository.currentUser.isNotEmpty
-              ? AppState.authenticated(authenticationRepository.currentUser)
+  AppBloc()
+      : super(
+          serviceLocator<AuthenticationRepository>().currentUser.isNotEmpty
+              ? AppState.authenticated(
+                  serviceLocator<AuthenticationRepository>().currentUser)
               : const AppState.unauthenticated(),
         ) {
     on<_AppUserChanged>(_onUserChanged);
     on<AppLogoutRequested>(_onLogoutRequested);
-    _userSubscription = _authenticationRepository.user.listen(
-      (user) => add(_AppUserChanged(user)),
-    );
+    _userSubscription = serviceLocator<AuthenticationRepository>().user.listen(
+          (user) => add(_AppUserChanged(user)),
+        );
   }
 
-  final AuthenticationRepository _authenticationRepository;
   late final StreamSubscription<User> _userSubscription;
 
   void _onUserChanged(_AppUserChanged event, Emitter<AppState> emit) {
@@ -34,7 +35,7 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   }
 
   void _onLogoutRequested(AppLogoutRequested event, Emitter<AppState> emit) {
-    unawaited(_authenticationRepository.logOut());
+    unawaited(serviceLocator<AuthenticationRepository>().logOut());
   }
 
   @override
