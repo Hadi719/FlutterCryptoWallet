150308c95a0e32f8d87efd70685f3cc002ca9521
Hadi <hadim69@gmail.com>
2023-12-27 17:01:27 +0330

    add HelperScaffoldSnackBar.

---
+++ lib/src/presentation/widgets/helper_scaffold_snack_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+/// Helper class to show a [SnackBar] using the passed context.
+///
+/// hides existing [SnackBar] and show the new one.
+class HelperScaffoldSnackBar {
+  HelperScaffoldSnackBar(this._context);
+
+  /// The scaffold of current context.
+  factory HelperScaffoldSnackBar.of(BuildContext context) {
+    return HelperScaffoldSnackBar(context);
+  }
+
+  final BuildContext _context;
+
+  /// Helper method to show a [SnackBar].
+  void show(String message) {
+    ScaffoldMessenger.of(_context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          content: Text(message),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+  }
+}
