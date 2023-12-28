cbd0aef25a6f18a413fcdcd020270f3de4c0e174
Hadi <hadim69@gmail.com>
2023-12-28 13:51:20 +0330

    Use app view.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'src/coinex_app.dart';
-import 'src/presentation/app/bloc_observer.dart';
+import 'src/presentation/app/app.dart';
 import 'src/service_locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Add bloc observer for dev debugging.
+  Bloc.observer = const AppBlocObserver();
 
   // Setup GetIt service locator.
   await setup();
 
-  // Add bloc observer for dev debugging.
-  Bloc.observer = const AppBlocObserver();
-
-  runApp(CoinExApp());
+  runApp(const App());
 }
