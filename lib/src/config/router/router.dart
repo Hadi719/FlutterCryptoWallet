5cac32348b72d48246b9b950a090e5c46b0095bc
Hadi <hadim69@gmail.com>
2024-01-05 17:42:17 +0330

    Add screen view to firebase analytics observer.

--- lib/src/config/router/router.dart
+++ lib/src/config/router/router.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../presentation/app/bloc/app_bloc.dart';
 import '../../presentation/auth/auth.dart';
 import '../../presentation/profile/profile.dart';
+import '../../service_locator.dart';
 
 List<Page<dynamic>> onGenerateAppViewPages(
   AppStatus state,
@@ -10,8 +12,16 @@ List<Page<dynamic>> onGenerateAppViewPages(
 ) {
   switch (state) {
     case AppStatus.authenticated:
+      _setCurrentScreen(ProfilePage.screenName);
       return [ProfilePage.page()];
     case AppStatus.unauthenticated:
+      _setCurrentScreen(AuthPage.screenName);
       return [AuthPage.page()];
   }
+}
+
+void _setCurrentScreen(String routeName) {
+  serviceLocator<FirebaseAnalytics>().setCurrentScreen(
+    screenName: routeName,
+  );
 }
