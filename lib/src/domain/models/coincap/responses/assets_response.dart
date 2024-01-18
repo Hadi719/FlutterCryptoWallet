869af415b96c12e2cc92a57a5a9052b7fb2984c4
Hadi <hadim69@gmail.com>
2024-01-18 20:25:11 +0330

    Add CoinCap Assets.

---
+++ lib/src/domain/models/coincap/responses/assets_response.dart
@@ -0,0 +1,74 @@
+import 'package:equatable/equatable.dart';
+
+/// The asset price is a volume-weighted average calculated by collecting ticker
+/// data from exchanges.
+///
+/// Each exchange contributes to this price in relation to their volume,
+/// meaning higher volume exchanges have more affect on this global price.
+/// All values are translated into USD (United States Dollar) and can be
+/// translated into other units of measurement through the /rates endpoint
+class AssetsResponse extends Equatable {
+  final String? id;
+  final String? rank;
+  final String? symbol;
+  final String? name;
+  final String? supply;
+  final String? maxSupply;
+  final String? marketCapUsd;
+  final String? volumeUsd24Hr;
+  final String? priceUsd;
+  final String? changePercent24Hr;
+  final String? vwap24Hr;
+  final String? explorer;
+
+  const AssetsResponse({
+    this.id,
+    this.rank,
+    this.symbol,
+    this.name,
+    this.supply,
+    this.maxSupply,
+    this.marketCapUsd,
+    this.volumeUsd24Hr,
+    this.priceUsd,
+    this.changePercent24Hr,
+    this.vwap24Hr,
+    this.explorer,
+  });
+
+  factory AssetsResponse.fromJson(Map<String, dynamic> json) {
+    return AssetsResponse(
+      id: json['id'],
+      rank: json['rank'],
+      symbol: json['symbol'],
+      name: json['name'],
+      supply: json['supply'],
+      maxSupply: json['maxSupply'],
+      marketCapUsd: json['marketCapUsd'],
+      volumeUsd24Hr: json['volumeUsd24Hr'],
+      priceUsd: json['priceUsd'],
+      changePercent24Hr: json['changePercent24Hr'],
+      vwap24Hr: json['vwap24Hr'],
+      explorer: json['explorer'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        id,
+        rank,
+        symbol,
+        name,
+        supply,
+        maxSupply,
+        marketCapUsd,
+        volumeUsd24Hr,
+        priceUsd,
+        changePercent24Hr,
+        vwap24Hr,
+        explorer,
+      ];
+
+  @override
+  bool get stringify => true;
+}
---
+++ lib/src/domain/models/coincap/responses/responses.dart
@@ -0,0 +1,1 @@
+export 'assets_response.dart';
