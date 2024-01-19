4f35443927356ed079663f9024d884e404b7fcb1
Hadi <hadim69@gmail.com>
2024-01-19 21:04:39 +0330

    rename.

--- lib/src/domain/models/coincap/responses/assets/assets.dart
+++ lib/src/domain/models/coincap/responses/assets/assets.dart
@@ -1,5 +1,5 @@
+export 'asset_histories_response.dart';
 export 'asset_history.dart';
-export 'asset_history_response.dart';
 export 'asset_market.dart';
 export 'asset_markets_response.dart';
 export 'asset_response.dart';
