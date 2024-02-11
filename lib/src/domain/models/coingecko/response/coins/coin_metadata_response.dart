d461691f2c91b6759a2cb24df6b145daf5b0358d
Hadi <hadim69@gmail.com>
2024-02-11 15:39:02 +0330

    Remove unnecessary jsonKeys.

--- lib/src/domain/models/coingecko/response/coins/coin_metadata_response.dart
+++ lib/src/domain/models/coingecko/response/coins/coin_metadata_response.dart
@@ -6,23 +6,23 @@ part 'coin_metadata_response.g.dart';
 @freezed
 class CoinMetadataResponse with _$CoinMetadataResponse {
   const factory CoinMetadataResponse(
-    @JsonKey(name: 'id') String? id,
-    @JsonKey(name: 'symbol') String? symbol,
-    @JsonKey(name: 'name') String? name,
+    String? id,
+    String? symbol,
+    String? name,
     @JsonKey(name: 'web_slug') String? webSlug,
     @JsonKey(name: 'asset_platform_id') dynamic assetPlatformId,
-    @JsonKey(name: 'platforms') Map<String, dynamic> platforms,
+    Map<String, dynamic> platforms,
     @JsonKey(name: 'detail_platforms') Map<String, dynamic> detailPlatforms,
     @JsonKey(name: 'block_time_in_minutes') int? blockTimeInMinutes,
     @JsonKey(name: 'hashing_algorithm') String? hashingAlgorithm,
-    @JsonKey(name: 'categories') List<String> categories,
+    List<String> categories,
     @JsonKey(name: 'preview_listing') bool? previewListing,
     @JsonKey(name: 'public_notice') dynamic publicNotice,
     @JsonKey(name: 'additional_notices') dynamic additionalNotices,
-    @JsonKey(name: 'localization') Map<String, dynamic> localization,
-    @JsonKey(name: 'description') Map<String, dynamic> description,
-    @JsonKey(name: 'links') Map<String, dynamic> links,
-    @JsonKey(name: 'image') Map<String, dynamic> image,
+    Map<String, dynamic> localization,
+    Map<String, dynamic> description,
+    Map<String, dynamic> links,
+    Map<String, dynamic> image,
     @JsonKey(name: 'country_origin') String? countryOrigin,
     @JsonKey(name: 'genesis_date') String? genesisDate,
     @JsonKey(name: 'sentiment_votes_up_percentage')
@@ -36,7 +36,7 @@ class CoinMetadataResponse with _$CoinMetadataResponse {
     @JsonKey(name: 'developer_data') Map<String, dynamic> developerData,
     @JsonKey(name: 'status_updates') List<dynamic> statusUpdates,
     @JsonKey(name: 'last_updated') String? lastUpdated,
-    @JsonKey(name: 'tickers') List<dynamic> tickers,
+    List<dynamic> tickers,
   ) = _CoinMetadataResponse;
 
   factory CoinMetadataResponse.fromJson(Map<String, dynamic> json) =>
