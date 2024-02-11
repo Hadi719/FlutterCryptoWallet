23fd4a94ac5c143c0143cb36dddee7de02664c37
Hadi <hadim69@gmail.com>
2024-02-11 15:09:20 +0330

    Remove unused code.

--- lib/src/domain/models/coingecko/response/simple/simple_supported_vs_currencies_response.dart
+++ lib/src/domain/models/coingecko/response/simple/simple_supported_vs_currencies_response.dart
@@ -13,23 +13,3 @@ class SimpleSupportedVsCurrenciesResponse
   factory SimpleSupportedVsCurrenciesResponse.fromJson(json) =>
       _$SimpleSupportedVsCurrenciesResponseFromJson(json);
 }
-
-/*class SimpleSupportedVsCurrenciesResponse extends Equatable {
-  const SimpleSupportedVsCurrenciesResponse({this.currenciesList = const []});
-
-  final List<String> currenciesList;
-
-  factory SimpleSupportedVsCurrenciesResponse.fromMap(List<dynamic> mapData) {
-    List<String> data =
-        List.from(mapData).map((dynamic e) => e as String).toList();
-    return SimpleSupportedVsCurrenciesResponse(
-      currenciesList: data,
-    );
-  }
-
-  @override
-  List<Object?> get props => [currenciesList];
-
-  @override
-  bool get stringify => true;
-}*/
