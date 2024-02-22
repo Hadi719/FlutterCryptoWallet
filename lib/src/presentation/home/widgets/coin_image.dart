767073f956403c2ad789bd42f756dbe9d8db9880
Hadi <hadim69@gmail.com>
2024-02-22 20:12:24 +0330

    Add coin image widget

---
+++ lib/src/presentation/home/widgets/coin_image.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/// Todo: Add default image.
+class CoinImage extends StatelessWidget {
+  final String? imageUrl;
+  final double height;
+  final double width;
+  const CoinImage({
+    super.key,
+    this.imageUrl,
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String url = '';
+    if (imageUrl != null && imageUrl!.isNotEmpty) {
+      url = imageUrl!;
+    }
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: url.isNotEmpty
+          ? Image(
+              image: ResizeImage(
+                NetworkImage(url),
+                height: height.toInt(),
+                width: width.toInt(),
+              ),
+              height: height,
+              width: width,
+            )
+          : FlutterLogo(size: height),
+    );
+  }
+}
