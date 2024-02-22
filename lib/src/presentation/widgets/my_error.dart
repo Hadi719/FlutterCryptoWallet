a4f94146cf1c070c581616ef4487889a062b832d
Hadi <hadim69@gmail.com>
2024-02-22 14:39:01 +0330

    Add error widget

---
+++ lib/src/presentation/widgets/my_error.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyError extends StatelessWidget {
+  final String error;
+
+  const MyError({
+    super.key,
+    required this.error,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        error,
+        style: const TextStyle(color: Colors.red),
+      ),
+    );
+  }
+}
