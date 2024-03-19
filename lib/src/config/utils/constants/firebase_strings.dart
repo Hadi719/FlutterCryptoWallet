71ffabae0a5c64ec7861517c0fbb6730c495ff30
Hadi <hadim69@gmail.com>
2024-03-19 16:30:27 +0330

    Add firebase storage strings

--- lib/src/config/utils/constants/firebase_strings.dart
+++ lib/src/config/utils/constants/firebase_strings.dart
@@ -1,1 +1,21 @@
-
+const String kStorageNameAPIsData = 'APIsData';
+const String kStoragePathAPIsData = 'APIsData';
+const String kStorageNameGecko = 'Gecko';
+const String kStoragePathGecko = '$kStoragePathAPIsData/Gecko';
+const String kStorageNameGeckoCoins = 'Coins';
+const String kStoragePathGeckoCoins = '$kStoragePathGecko/Coins';
+const String kStorageNameGeckoHistory = 'History';
+const String kStoragePathGeckoHistory = '$kStoragePathGeckoCoins/History';
+const String kStorageNameGeckoMarketChart = 'MarketChart';
+const String kStoragePathGeckoMarketChart =
+    '$kStoragePathGeckoCoins/MarketChart';
+const String kStorageNameGeckoMarketChartRange = 'MarketChartRange';
+const String kStoragePathGeckoMarketChartRange =
+    '$kStoragePathGeckoCoins/MarketChartRange';
+const String kStorageNameGeckoMetadata = 'Metadata';
+const String kStoragePathGeckoMetadata = '$kStoragePathGeckoCoins/Metadata';
+const String kStorageNameGeckoOHLC = 'OHLC';
+const String kStoragePathGeckoOHLC = '$kStoragePathGeckoCoins/OHLC';
+const String kStorageNameGeckoMarketsList = 'MarketsList';
+const String kStoragePathGeckoMarketsList =
+    '$kStoragePathGeckoCoins/MarketsList';
